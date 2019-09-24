4e5df17682a5716b1d7aefef8b32b1930c793aad
winty <18814127231@163.com>
2019-09-24 19:58:04 +0800

    feat: add tpl

---
+++ dist/static/chunk.main.b931c983.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{121:function(n,o,p){n.exports=p(122)}},[[121,1,2]]]);
---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AppContainer = () => (
+  <div style={{
+    fontSize: '28px',
+    margin: '50px auto',
+    textAlign: 'center',
+  }}
+  >
+  Hello World
+  </div>
+)
+
+export default AppContainer
